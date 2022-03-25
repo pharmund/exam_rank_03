d653454110cf4128d5b2b740617b02035b14650d
Alexander Portnov <86096084+pharmund@users.noreply.github.com>
2022-03-25 23:50:42 +0300

    25.03.2022
    25.03.2022

--- mini_paint.c
+++ mini_paint.c
@@ -14,7 +14,7 @@ float x_start, y_start, r;
 void putstr(char *s) {
     while (*s)
         write(1, s++, 1);
-    write(1, "\n", 1)
+    write(1, "\n", 1);
 }
 
 int draw(char type, float x_start, float y_start, float r, char c) {
